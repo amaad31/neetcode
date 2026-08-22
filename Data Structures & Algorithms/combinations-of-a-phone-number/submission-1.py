9941d1d53a57148da20c81cbf6a2c07ac63810da
Amaad <amaadk07@gmail.com>
2026-08-22 13:44:14 +0200

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        keypad = {2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wxyz"}
+        res = []
+        def dfs(i: int, cur_res: str) -> None:
+            if i == len(digits):
+                res.append(cur_res)
+                return
+            
+            for ch in keypad[int(digits[i])]:
+                cur_res += ch
+                dfs(i + 1, cur_res)
+                cur_res = cur_res[:-1]
+            
+        dfs(0, "")
+        return res
