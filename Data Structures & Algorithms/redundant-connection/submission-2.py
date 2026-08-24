e768abd8b4b916e19d312235b453ee2bea876181
Amaad <amaadk07@gmail.com>
2026-08-24 02:50:56 +0200

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        if not edges:
+            return []
+        
+        edges_map = defaultdict(list)
+        visited = set()
+        res = set()
+
+        for edge in edges:
+            edges_map[edge[0]].append(edge[1])
+            edges_map[edge[1]].append(edge[0])
+        
+        def dfs(node, prev, path):
+            if node in visited:
+                nonlocal res
+                res = path
+                return False
+            
+            visited.add(node)
+            path.add(node)
+            for neighbor in edges_map[node]:
+                if neighbor == prev:
+                    continue
+                if not dfs(neighbor, node, path):
+                    return False
+            path.remove(node)
+            return True
+
+        for node in edges_map.keys():
+            if node not in visited:
+                if not dfs(node, None, set()):
+                    break
+        for i in range(len(edges) - 1, -1, -1):
+            x, y = edges[i]
+            if x in res and y in res:
+                return [x, y]
+        
+        return []
