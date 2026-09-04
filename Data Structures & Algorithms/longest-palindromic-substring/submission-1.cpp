86de01dacc67d4730be2dfc854f24eac3ce9f1b0
Amaad <amaadk07@gmail.com>
2026-09-04 11:52:32 +0200

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int resLen = -1;
+        string res = "";
+
+        for (int i = 0; i < s.size(); i++){
+            // odd len
+            int leftPtr = i;
+            int rightPtr = i;
+            while(leftPtr >= 0 && rightPtr < s.size() && s[leftPtr] == s[rightPtr]){
+                if ((rightPtr - leftPtr + 1) > resLen) {
+                    resLen = rightPtr - leftPtr + 1;
+                    res = s.substr(leftPtr, rightPtr - leftPtr + 1);
+                }
+                leftPtr -= 1;
+                rightPtr += 1;
+            }
+            
+            // even len
+            leftPtr = i;
+            rightPtr = i + 1;
+            while(leftPtr >= 0 && rightPtr < s.size() && s[leftPtr] == s[rightPtr]){
+                if ((rightPtr - leftPtr + 1) > resLen) {
+                    resLen = rightPtr - leftPtr + 1;
+                    res = s.substr(leftPtr, rightPtr - leftPtr + 1);
+                }
+                leftPtr -= 1;
+                rightPtr += 1;
+            }
+        }
+        return res;
+    }
+};
